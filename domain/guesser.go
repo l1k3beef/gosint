7120dc124e05d24f073e463c5677d0b5a85c123a
l1k3beef <l1k3beef@outlook.com>
2022-12-08 08:15:47 +0800

    更新域名猜测

--- domain/guesser.go
+++ domain/guesser.go
@@ -1,5 +1,7 @@
 package domain
 
+import "strings"
+
 type DomainGuesser struct {
 	*DomainModule
 }
@@ -14,7 +16,19 @@ func (dg *DomainGuesser) UseDict() {
 
 // UseCharacterTraversal 逐字符遍历
 func (dg *DomainGuesser) UseCharacterTraversal() {
-
+	success := make(map[string]struct{})
+	for k, _ := range success {
+		last := strings.Index(k, dg.RootDomain)
+		for i := 0; i < last; i++ {
+			if k[i] == byte('.') {
+				continue
+			}
+			for _, c := range "abcdefghijklmnopqrstuvwxyz-" {
+				next := k[:i] + string(c) + k[i:]
+				println(next)
+			}
+		}
+	}
 }
 
 // UsePermutations 排列组合
